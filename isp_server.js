7ff7b0f1a353a663b4194aa652a975a7aa0b7f1a
barteksocz <barteksocz@wp.pl>
2018-01-13 17:52:31 +0100

    Integrating server with smartstorm, part 2

--- isp_server.js
+++ isp_server.js
@@ -14,12 +14,12 @@ _server.post("/sensors_data",function(req,res) {
 	request({
 	url: 'http://alfa.smartstorm.io/api/v1/measure',
 	method: 'POST',
-	json: {"user_id":"esp8266@test.com","sensor_id":"5a5a34e7498f190f7a9ef50b","desc":"temperature","measure_value":20}
+	json: {"user_id":"esp8266@test.com","sensor_id":"5a5a34e7498f190f7a9ef50b","desc":"temperature","measure_value":req.body.temperature}
 	});
 	request({
 	url: 'http://alfa.smartstorm.io/api/v1/measure',
 	method: 'POST',
-	json: {"user_id":"esp8266@test.com","sensor_id":"5a5a34f2498f190f7a9ef50c","desc":"magnetic sensor state","measure_value":1}
+	json: {"user_id":"esp8266@test.com","sensor_id":"5a5a34f2498f190f7a9ef50c","desc":"magnetic sensor state","measure_value":req.body.magnetic}
 	});	
 	res.send("ACK");	
 });
