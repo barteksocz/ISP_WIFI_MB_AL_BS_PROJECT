550523f5fb93682f1e6af29276be5ec093c0fe28
barteksocz <barteksocz@wp.pl>
2018-01-13 17:44:07 +0100

    Integrating server with smartstorm, part 1.

--- isp_server.js
+++ isp_server.js
@@ -3,61 +3,24 @@ var os = require("os");
 var fs = require("fs");
 var bodyParser = require('body-parser');
 var sleeper = require('system-sleep');
+var request = require('request');
 
 var _server = express();
-
-function isp_wait(){
-	sleeper(10);
-}
 
 _server.use(bodyParser.urlencoded({extended:true}));
 _server.listen(80,'127.0.0.1');
 
 _server.post("/sensors_data",function(req,res) {
-	
-	fs.writeFile("sensors_data.txt","T",function(err){
-		if (err) console.log("error during writing to file");
+	request({
+	url: 'http://alfa.smartstorm.io/api/v1/measure',
+	method: 'POST',
+	json: {"user_id":"esp8266@test.com","sensor_id":"5a5a34e7498f190f7a9ef50b","desc":"temperature","measure_value":20}
 	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",os.EOL,function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",req.body.temperature,function(err){
-		if(err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",os.EOL,function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt","M",function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",os.EOL,function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",req.body.magnetic,function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",os.EOL,function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt","D",function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",os.EOL,function(err){
-		if (err) console.log("error during writing to file");
-	});
-	isp_wait();
-	fs.appendFile("sensors_data.txt",req.body.distance,function(err){
-		if (err) console.log("error during writing to file");
-	});
+	request({
+	url: 'http://alfa.smartstorm.io/api/v1/measure',
+	method: 'POST',
+	json: {"user_id":"esp8266@test.com","sensor_id":"5a5a34f2498f190f7a9ef50c","desc":"magnetic sensor state","measure_value":1}
+	});	
 	res.send("ACK");	
 });
 
